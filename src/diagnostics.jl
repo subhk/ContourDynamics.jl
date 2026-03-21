dbce05d19fa586676d969b179d34375c5b3bb55a
Subhajit Kar <subhajitkar19@gmail.com>
2026-03-21 08:14:17 +0000

    Update diagnostics.jl

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -79,7 +79,10 @@ function ellipse_moments(c::PVContour{T}) where {T}
     lambda1 = trace / 2 + disc
     lambda2 = trace / 2 - disc
 
-    aspect_ratio = sqrt(max(one(T), lambda1 / max(lambda2, eps(T))))
+    # Guard against near-degenerate contours: use a threshold relative to
+    # the trace (sum of eigenvalues) so that the aspect ratio stays finite.
+    lambda2_safe = max(lambda2, trace * eps(T) * T(100))
+    aspect_ratio = sqrt(max(one(T), lambda1 / lambda2_safe))
     angle = T(0.5) * atan(2 * Jxy, Jxx - Jyy)
 
     return (aspect_ratio, angle)
