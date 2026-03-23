3c99e720cd0a1014cfa6de04b8dd493e8a7a74bf
Subhajit Kar <subhajitkar19@gmail.com>
2026-03-23 17:41:31 +0000

    Update test_utils.jl

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -12,3 +12,11 @@ function elliptical_patch(a::T, b::T, N::Int, pv::T) where {T<:AbstractFloat}
     nodes = [SVector{2,T}(a * cos(2π * i / N), b * sin(2π * i / N)) for i in 0:(N-1)]
     return PVContour(nodes, pv)
 end
+
+"""Create a rotated elliptical PVContour with N nodes, semi-axes a and b, rotated by angle θ."""
+function rotated_elliptical_patch(a::T, b::T, N::Int, pv::T, θ::T) where {T<:AbstractFloat}
+    cosθ, sinθ = cos(θ), sin(θ)
+    nodes = [SVector{2,T}(a * cos(2π * i / N) * cosθ - b * sin(2π * i / N) * sinθ,
+                           a * cos(2π * i / N) * sinθ + b * sin(2π * i / N) * cosθ) for i in 0:(N-1)]
+    return PVContour(nodes, pv)
+end
