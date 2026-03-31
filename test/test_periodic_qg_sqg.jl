35102e2022f584cc3e685a9845e38f2b759b206c
Subhajit Kar <subhajitkar19@gmail.com>
2026-03-31 11:12:31 +0100

    Update test_periodic_qg_sqg.jl

--- test/test_periodic_qg_sqg.jl
+++ test/test_periodic_qg_sqg.jl
@@ -86,7 +86,8 @@ extended = get(ENV, "CONTOURDYNAMICS_EXTENDED_TESTS", "false") == "true"
 
     @testset "Multi-layer periodic energy" begin
         Ld = SVector(1.0)
-        coupling = SMatrix{2,2}(-1.0, 1.0, 1.0, -1.0)
+        F = 1.0 / (2 * Ld[1]^2)
+        coupling = SMatrix{2,2}(-F, F, F, -F)
         kernel = MultiLayerQGKernel(Ld, coupling)
 
         c1 = circular_patch(0.3, 32, 1.0)
