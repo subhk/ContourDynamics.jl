9e553ed63bcdf7ac6650e1949ccf9e891065733c
Subhajit Kar <subhajitkar19@gmail.com>
2026-04-09 18:26:37 +0100

    Update test_utils.jl

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,17 +1,8 @@
 using ContourDynamics
 using StaticArrays
 
-"""Create a circular PVContour with N nodes, radius R, centered at origin."""
-function circular_patch(R::T, N::Int, pv::T) where {T<:AbstractFloat}
-    nodes = [SVector{2,T}(R * cos(2π * i / N), R * sin(2π * i / N)) for i in 0:(N-1)]
-    return PVContour(nodes, pv)
-end
-
-"""Create an elliptical PVContour with N nodes, semi-axes a and b, centered at origin."""
-function elliptical_patch(a::T, b::T, N::Int, pv::T) where {T<:AbstractFloat}
-    nodes = [SVector{2,T}(a * cos(2π * i / N), b * sin(2π * i / N)) for i in 0:(N-1)]
-    return PVContour(nodes, pv)
-end
+# circular_patch and elliptical_patch are now exported from ContourDynamics.
+# Only rotated_elliptical_patch remains here (not exported, test-only helper).
 
 """Create a rotated elliptical PVContour with N nodes, semi-axes a and b, rotated by angle θ."""
 function rotated_elliptical_patch(a::T, b::T, N::Int, pv::T, θ::T) where {T<:AbstractFloat}
