a835b017f9fbf581582fc7d76eb7e9a730cfaa16
Subhajit Kar <subhajitkar19@gmail.com>
2026-08-09 19:48:28 +0300

    Update test_recorded_arrays.jl

--- test/test_recorded_arrays.jl
+++ test/test_recorded_arrays.jl
@@ -10,8 +10,9 @@ using Test, ContourDynamics, RecordedArrays
     @test_throws ArgumentError recorded_diagnostics(
         prob; dt=0.01, nsteps=2, record_every=0)
 
-    contour32 = circular_patch(0.25f0, 16, 1.0f0)
+    contour32 = circular_patch(0.25, 16, 1.0; T=Float32)
     prob32 = ContourProblem(EulerKernel(), UnboundedDomain(), [contour32])
+    @test eltype(prob32.contours[1].nodes[1]) === Float32
     @test_throws ArgumentError recorded_diagnostics(prob32; dt=1.0e100, nsteps=2)
 
     rec = recorded_diagnostics(prob; dt=0.01, nsteps=2, record_every=1)
